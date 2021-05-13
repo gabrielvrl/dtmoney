e6698625962c7f33d8060497668f29c425a5d3e1
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-05-12 23:21:04 -0300

    Global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: #8257e6;
-`
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello Rocketseat</Title>
+      <h1>Hello Rocketseat</h1>
+      <GlobalStyle />
     </div>
   );
 }
