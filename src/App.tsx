d575ab4363ecad2544cdc856b337d08d225765ec
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-05-12 23:04:47 -0300

    adding style components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello Wolrd</h1>
+      <Title>Hello Rocketseat</Title>
     </div>
   );
 }
