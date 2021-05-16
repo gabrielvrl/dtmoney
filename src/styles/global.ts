07e2e43fcc56f91a8898b40939185dfb64d9b329
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-05-16 12:08:41 -0300

    Dashboard and Summary Component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   :root {
     --red: #e52e4d;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
